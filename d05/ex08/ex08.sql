099e144719914c838bd93ac529deeb539656a8cf
Ivan BOHONOSIUK <ibohonos@e2r2p2.unit.ua>
2018-04-10 19:58:00 +0300

    d05 fix ex03, 08, 10 add ex11 - ex18

--- d05/ex08/ex08.sql
+++ d05/ex08/ex08.sql
@@ -1,3 +1,4 @@
-SELECT last_name, first_name, DATE_FORMAT(birthday, '%Y-%m-%d') FROM user_card
-WHERE EXTRACT(YEAR FROM birthday) = 1982
+SELECT last_name, first_name, DATE_FORMAT(birthdate, '%Y-%m-%d')
+AS 'birthdate' FROM user_card
+WHERE YEAR(birthdate) = 1989
 ORDER BY last_name ASC;
